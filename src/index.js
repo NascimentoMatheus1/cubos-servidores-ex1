caab8cafacc2440c1e9a999d9fd6b56a8f850814
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2025-08-24 14:50:31 -0300

    utilizando um intermediario para autorizar a entrada

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ const express = require('express');
 
 const app = express();
 
+const primeiroIntermediario = (req, res, next) => {
+    const { senha } = req.query;
+    if(senha && senha === "123"){
+        next()
+    }else{
+        res.send('Usuario não autorizado!');
+    }
+}
+
+app.use(primeiroIntermediario);
+
 app.get('/listagem/:id', controladorGeral); 
 
 app.get('/listagem/', controladorFiltrarCarros); 
